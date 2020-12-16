7e82dbc6fb56445d821f2c523dd107a6919063e7
Roman Shentsev <3184447@mail.ru>
2020-12-16 16:47:11 +0300

    homework 9: task 3 and tests.

--- lecture_09/homework9/tasks/task3.py
+++ lecture_09/homework9/tasks/task3.py
@@ -15,6 +15,40 @@ from typing import Callable, Optional
 
 
 def universal_file_counter(
-    dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
+    directory_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
 ) -> int:
-    pass
+    """If there are no tokenizer, it will counts lines in all files in directory_path
+        with given file_extension.
+
+    If given tokenizer, it counts tokens.
+
+    Using:
+        For dir with two files from hw1.py:
+        >>> universal_file_counter(test_dir, "txt")
+        6
+        >>> universal_file_counter(test_dir, "txt", str.split)
+        6
+
+    Args:
+        directory_path: Path object pointing to directory for count;
+        file_extension: type of files for search in directory_path;
+        tokenizer: optional function for work with lines of found files. Defaults to
+        None.
+
+    Returns:
+        amount of lines or tokens in found files.
+
+    """
+
+    files_iterator = directory_path.glob(f"*.{file_extension}")
+    token_count = 0
+    for file_path in files_iterator:
+        with file_path.open() as file:
+            if tokenizer:
+                token_iterable = map(tokenizer, (line for line in file))
+                for item in token_iterable:
+                    token_count += len(item)
+            else:
+                for _ in file:
+                    token_count += 1
+    return token_count
